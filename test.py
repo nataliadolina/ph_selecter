c13ad0d01a91a8164eda08271764f3b7211e308d
nataliadolina <45314570+nataliadolina@users.noreply.github.com>
2023-02-13 20:40:02 +0300

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import re
-
-test = "-DSCF0614.jpg, DSCF0596.RAF errgeg\nDSCF0637\tDSCF0606dewwwwwwwwwwww"
-result = re.findall("\d{4}", test)
-test1 = "-DSCF0614.jpg"
-result1 = re.findall("\d{4}", test1)
-print(result)
